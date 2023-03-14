71c4dbb9d46b7db7afd0f1d61f269dd60ac56762
md-golap <golapmax@gmail.com>
2023-03-14 22:55:26 +0600

    change

--- test.php
+++ test.php
@@ -1,6 +1,14 @@
 <?php
+ function multiply($variable){
+    for($start = 1; $start < 10; $start++){
+        echo $variable . 'x' . $start . '=' . $variable * $start . '<br/>';
+    }
+ }
+ multiply(5);
+ multiply(2);
+ multiply(6);
 
-
-echo 'php basic';
 
 ?>
+
+
