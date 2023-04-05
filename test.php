02a2beceddf78070262ba53f9d7c2057244ee0ff
md-golap <golapmax@gmail.com>
2023-04-05 11:51:52 -0700

    change

--- test.php
+++ test.php
@@ -1,14 +1,23 @@
 <?php
- function multiply($variable){
-    for($start = 1; $start < 10; $start++){
-        echo $variable . 'x' . $start . '=' . $variable * $start . '<br/>';
-    }
+ $arry =[12, 3299,99,99,99,877];
+
+
+
+ foreach($arry as $value){
+
+    // $sum  = $value;
  }
- multiply(5);
- multiply(2);
- multiply(6);
+
+ $temp = "ggg";
+ $temp = "oooo";
+ $temp = "hhh";
+ $temp = "llll";
+
+
+
+ echo $temp;
      
 
-?>
 
+     
 
