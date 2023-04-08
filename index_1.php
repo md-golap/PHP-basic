7e01316fa287b17001f2ab66d625b1dc452e3215
md-golap <golapmax@gmail.com>
2023-04-08 02:45:15 -0700

    add

--- index_1.php
+++ index_1.php
@@ -1,6 +1,46 @@
 <?php 
 
-echo "one";
+//while loop
+
+$start=1;
+while($start<=10){
+    echo '2*'.$start. '='. 2*$start.'</br>';
+    $start++;
+
+}
+
+// for loop;
+
+
+for($start=1; $start<=10; $start++){
+
+    echo '8*'.$start.'='. 8*$start. '</br>';
+}
+
+
+//array element;
+
+$arry=[1,3,2,5,6,87,53,54];
+
+$arry_start=0;
+
+while($arry_start<=5){
+
+    echo $arry[$arry_start].'</br>';
+    $arry_start++;
+
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 ?>
