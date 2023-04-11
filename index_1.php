a845ff26921e2a7fd73eacae7204e88d7c5e827d
md-golap <golapmax@gmail.com>
2023-04-11 14:12:36 -0700

    add

--- index_1.php
+++ index_1.php
@@ -18,7 +18,7 @@ for($start=1; $start<=10; $start++){
 }
 
 
-//array element;
+//array element while;
 
 $arry=[1,3,2,5,6,87,53,54];
 
@@ -31,14 +31,59 @@ while($arry_start<=5){
 
 }
 
+echo '</br>';
+
+//for
+
+for($array_ele=0; $array_ele<=7; $array_ele++){
+    echo $arry[$array_ele].'</br>';
+}
 
 
 
+echo '</br>';
+// array_foreach:
+
+
+$array_foreach =['sumon', 33, 'akbor', 66, 'akramuk', 99, 'locacation'];
 
 
 
+foreach($array_foreach as $value){
+
+    echo $value. '</br>';
+}
 
 
+echo '</br>';
+// array_foreach multidimention;
+
+$array_multidimention = [
+
+    'fast_name'=> 'jaed',
+    'last_name' => 'Mossharop',
+    'roll' => 3485,
+    'location' => [
+        'village'=> 'shoshalpur',
+        'zip_code'=> 5460,
+    ]
+
+];
+
+
+foreach($array_multidimention as $key=> $multidimention){
+
+    if(is_array($multidimention)){
+        foreach($multidimention as $key=> $value){
+            echo $key . ' '. $value.'</br>';
+        }
+    }else{
+        echo $key. ' '. $multidimention.'</br>';
+
+    }
+
+   
+}
 
 
 
